acab1f55846d83f179a174247ee195da26fb362d
freddy208 <kfreddypatient@gmail.com>
2025-11-14 15:30:02 +0100

    Fin endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::prefix('api')->group(function () {
-    require __DIR__.'/api.php';
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
